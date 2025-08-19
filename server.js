dd4d2fd426c6b9610e55b0c3e83b305290bf3468
Aman Burnwal <gpkabk@gmail.com>
2025-08-19 18:18:34 +0530

    added global error handler

--- server.js
+++ server.js
@@ -4,12 +4,14 @@ import cookieParser from 'cookie-parser';
 import connectDB from './config/connectDB.js';
 import userRouter from './routers/Users.js'
 import taskRouter from './routers/Tasks.js'
+import { errorHandler } from './middleware/errorHandler.js';
 
 dotenv.config();
 const app = express();
 
 app.use(json());
 app.use(cookieParser());
+app.use(errorHandler);
 app.use("/api/v1/auth", userRouter);
 app.use('/api/v1/task', taskRouter);
 const PORT = process.env.PORT || 3000;
