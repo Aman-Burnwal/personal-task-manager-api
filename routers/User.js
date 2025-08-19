679c0466f7ccf31fea828ef5d8ccd21aff17fdef
Aman Burnwal <gpkabk@gmail.com>
2025-08-19 12:28:41 +0530

    updated routes

--- routers/User.js
+++ routers/User.js
@@ -3,7 +3,7 @@ import { signup, login } from '../controllers/User.js';
 const router = Router();
 
 
-router.post('/auth/register', signup);
-router.post('/auth/login', login);
+router.post('/register', signup);
+router.post('/login', login);
 
 export default router;
