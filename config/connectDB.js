43bdc6c5be78be8e28d5a9b9d897d1706fe659af
Aman Burnwal <gpkabk@gmail.com>
2025-08-20 10:00:38 +0530

    commented the console

--- config/connectDB.js
+++ config/connectDB.js
@@ -5,16 +5,16 @@ const connectDB = async () => {
   try {
     await sequelize.authenticate();
     await sequelize.sync();
-    console.log('connected successfully to database')
+    // console.log('connected successfully to database')
   } catch (error) {
-    console.error('error in server connection', error.message);
+    // console.error('error in server connection', error.message);
     if (numberOfDatabaseConnectionTry < 10) {
       numberOfDatabaseConnectionTry++;
       setTimeout(connectDB, 5000);
-      console.log('Sever Connection will be retry in next 5 seconds');
+      // console.log('Sever Connection will be retry in next 5 seconds');
     }
     else {
-      console.log("Maximum database limit crossed");
+      // console.log("Maximum database limit crossed");
       process.exit(1);
     }
   }
